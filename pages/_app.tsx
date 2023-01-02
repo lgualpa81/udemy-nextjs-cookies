4a3f0d03d38cc78af90e6e078e2bba2a4bb5bc1d
Leonardo Gualpa <lgualpa81@gmail.com>
2023-01-02 15:56:05 -0500

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { darkTheme } from "../themes";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
---
+++ pages/theme-changer.tsx
@@ -0,0 +1,46 @@
+import {
+  Card,
+  CardContent,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
+import { ChangeEvent, useState } from "react";
+import { Layout } from "../components/layouts";
+
+const ThemeChangerPage = () => {
+  const [currentTheme, setCurrentTheme] = useState("light");
+  const onThemeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedTheme:string = event.target.value
+    console.log({selectedTheme})
+    setCurrentTheme(selectedTheme);
+  };
+  return (
+    <Layout>
+      <Card>
+        <CardContent>
+          <FormControl>
+            <FormLabel>Tema</FormLabel>
+            <RadioGroup row value={currentTheme} onChange={onThemeChange}>
+              <FormControlLabel
+                value="light"
+                control={<Radio />}
+                label="Light"
+              />
+              <FormControlLabel value="dark" control={<Radio />} label="Dark" />
+              <FormControlLabel
+                value="custom"
+                control={<Radio />}
+                label="Custom"
+              />
+            </RadioGroup>
+          </FormControl>
+        </CardContent>
+      </Card>
+    </Layout>
+  );
+};
+
+export default ThemeChangerPage;
